63f34ee3607d75966449c28e5140967ad08f83ee
Minghao Tang <minghao@emperor-07.cs.wisc.edu>
2020-04-02 19:27:54 -0500

    mapper finish

--- p4a/wordcount.c
+++ p4a/wordcount.c
@@ -11,7 +11,7 @@
 
 void Map(char *file_name)
 {
-    printf("this is the beginning of map\n");
+   // printf("this is the beginning of map\n");
     FILE *fp = fopen(file_name, "r");
     assert(fp != NULL);
 
@@ -42,54 +42,53 @@ void Map(char *file_name)
 void Combine(char *key, CombineGetter get_next)
 {
 
-    printf("now in the combiner: \n" );
+  //  printf("now in the combiner: \n" );
     int count = 0;
     char *value;
 
     while ((value = get_next(key)) != NULL) // return the value
     {
         count++; // Emmited Map values are "1"s
-        printf("count %d\n", count);
+     // printf("count %d\n", count);
     }
-    // Convert integer (count) to string (value)
-    printf("count finish\n ");
 
     value = (char *)malloc(10 * sizeof(char));
     sprintf(value, "%d", count);
-    printf("hello001\n");
+//    printf("hello001\n");
     MR_EmitToReducer(key, value);
-
     free(value);
-
-    printf("mapper finish\n");
+   //printf("mapper finish\n");
 }
 
 // invoked once per intermediate key
 
+// each thread is responsible for a set of keys that are assigned to it
 void Reduce(char *key, ReduceStateGetter get_state,
             ReduceGetter get_next, int partition_number)
 {
-    // `get_state` is only being used for "eager mode" (explained later)
-    assert(get_state == NULL);
+    char *state = get_state(key, partition_number);
+    int count = (state != NULL) ? atoi(state) : 0;
 
-    int count = 0;
+    char *value = get_next(key, partition_number);
 
-    char *value;
-    while ((value = get_next(key, partition_number)) != NULL)
+    if (value != NULL)
     {
         count += atoi(value);
+        // Convert integer (count) to string (value)
+        value = (char *)malloc(10 * sizeof(char));
+        sprintf(value, "%d", count);
+
+        MR_EmitReducerState(key, value, partition_number);
+        free(value);
     }
-
-    // Convert integer (count) to string (value)
-    value = (char *)malloc(10 * sizeof(char));
-    sprintf(value, "%d", count);
-
-    printf("%s %s\n", key, value);
-    free(value);
+    else
+    {
+        printf("%s %d\n", key, count);
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    MR_Run(argc, argv, Map, 1, Reduce, 10, Combine, MR_DefaultHashPartition);
+    MR_Run(argc, argv, Map, 1, Reduce, 1, Combine, MR_DefaultHashPartition);
 
 }
