143b78258b5b7908a5370bfb0c9a0365932119bd
Minghao Tang <minghao@royal-15.cs.wisc.edu>
2020-04-21 23:32:27 -0500

    last

--- p5/FS_checker/xfsck.c
+++ p5/FS_checker/xfsck.c
@@ -21,6 +21,8 @@
 #undef stat
 #undef dirent
 
+typedef unsigned int uint;
+
 // mmap | map a file's contents to virtual memory //very powerful
 //-----------
 //file contents
@@ -41,9 +43,11 @@
 void *img_ptr; // the first addreess of block;
 struct dinode *dip;  //the address of inode
 struct superblock *sb; // the addreess of superblock
-int bitmap [2000];
-int bitnode [200];
+int bitmap [10000];
+int bitnode [2000];
 int Block_ptr=0;
+const char *s1 = ".";
+const char *s2 = "..";
 
 void error(char *msg)
 {
@@ -59,6 +63,7 @@ int Check_BitMap(int addr);
 void Map_bitmap();
 void Size_check(int blockcount, int dipsize);
 void Inode_check();
+void walk_dir_inode(struct dinode *dip);
 
 
 int main(int argc, char *argv[]){
@@ -89,7 +94,6 @@ int main(int argc, char *argv[]){
 
    // how can we access or parse this directories contents
    //struct xv6_dirent *dent = (struct xv6_dirent *)(img_ptr + dip[1].addrs[0] * BSIZE);
-
     check_SuperBlock();
     Map_bitmap();
     Inode_check();
@@ -126,7 +130,7 @@ int  walk_inode(struct dinode *dip){
     int NumBlocks = 0;
     for (int i =0;i<NDIRECT;i++){
         if(dip->addrs[i] != 0){
-           if((dip->addrs[i] >994)||(dip->addrs[i]<28)){
+           if((dip->addrs[i] >(sb->nblocks ))||(dip->addrs[i]<BBLOCK(0, sb->ninodes))){
                error("ERROR: bad direct address in inode.");
            }
            bitmap[dip->addrs[i]]++; //record the number
@@ -136,18 +140,18 @@ int  walk_inode(struct dinode *dip){
            NumBlocks++;
         }
     }
-    if(dip->addrs[12] == 0) {
+    if(dip->addrs[NDIRECT] == 0) {
     Size_check(NumBlocks,dip->size);
     return NumBlocks;
     }
-    bitmap[dip->addrs[12]]++;
+    bitmap[dip->addrs[NDIRECT]]++;
 
-    uint *indirect = (uint *)(img_ptr + (dip->addrs[12]) *BSIZE );
+    uint *indirect = (uint *)(img_ptr + (dip->addrs[NDIRECT]) *BSIZE );
    // printf("indirect %d\n", dip->addrs[12]);
     for (int i =0; i<NINDIRECT;i++){
 
         if (*indirect != 0){
-            if((*indirect > 994) || (*indirect < 28)){
+            if((*indirect > (sb->nblocks )) || (*indirect < BBLOCK(0, sb->ninodes))){
               error("ERROR: bad indirect address in inode.");
             }
             bitmap[*indirect]++; // count the reference of a block
@@ -181,15 +185,13 @@ void check_SuperBlock(){
 
     int total_block = 0;
     Dir_check(dip, 1);
-    bitmap[29]=1;
-    for ( int i =2; i<IPB*25;i++){
+    bitmap[BBLOCK(0, sb->ninodes)+1] = 1;
+
+    for ( int i =2; i<(sb->ninodes);i++){
         total_block+=walk_inode(dip +i);
          Dir_check(dip,i);
-    //   printf("the current block num is %d\n", total_block+28);
     }
-    if(total_block+28> sb->nblocks){
-        error("ERROR: superblock is corrupted");
-    }
+
   //  printf("the totoal data block used  is %d\n",total_block);
 //printf("the totoal block used  is %d\n", blocknum);
 }
@@ -238,8 +240,9 @@ int Check_BitMap(int addr){
   //  printf("byte: %d ,offset %d, %x\n", byte_num, bit_offset, *(bitmap + byte_num) & (1 << bit_offset));
 }
 void Map_bitmap(){
-    for (int i=29; i<1024;i++){
-    //    printf("the block num is %d, stat:%d, bitmap%d\n", i, bitmap[i], Check_BitMap(i));
+    for (int i = BBLOCK(0, sb->ninodes)+1; i < sb->nblocks; i++)
+    {
+            //printf("the block num is %d, stat:%d, bitmap%d\n", i, bitmap[i], Check_BitMap(i));
         if((bitmap[i]==0) && (Check_BitMap(i)==1)){
             error("ERROR: bitmap marks block in use but it is not in use.");
         }
@@ -257,68 +260,109 @@ void Size_check(int blockcount, int dipsize){
 
 }
 
+//static int ck9_inodes_used[1000];
 void Inode_check(){
 
+   //s int nblocks = sb->nblocks;
     int ninodes = sb->ninodes;
-
+    //int size = sb->size;
     int ck9_inodes_used[ninodes];
-    for (int i = 0; i < ninodes; ++ i) ck9_inodes_used[i] = 0;
-
-    int ck12_inodes_used[ninodes];
-    for (int i = 0; i < ninodes; ++ i) ck12_inodes_used[i] = 0;
 
 
-// record the usage
+    for (int i = 0; i < ninodes; ++i)
+        ck9_inodes_used[i] = 0;
 
     for (int i = 0; i < ninodes; ++i)
     {
-        if (dip[i].type == 1)
-        {
-            uint temp_addr = dip[i].addrs[0];
-            struct xv6_dirent *entry = (struct xv6_dirent *)(img_ptr + temp_addr * BSIZE);
-            int j = 1;
-            while (entry[j].inum != 0)
-            {
-                ck9_inodes_used[entry[j].inum]++;
-                j++;
+        if(dip[i].type == 1){
+            for (int k = 0; k < NDIRECT; ++ k){
+                if (dip[i].addrs[k] != 0){
+                    uint temp_addr = dip[i].addrs[k];
+                    struct xv6_dirent* entry = (struct xv6_dirent *)(img_ptr + temp_addr * BSIZE);
+                    for (int j = 0; j < BSIZE / sizeof(struct xv6_dirent); ++ j){
+                        if (entry[j].inum != 0 && strcmp(entry[j].name, s1) != 0 && strcmp(entry[j].name, s2) != 0)
+                            ck9_inodes_used[entry[j].inum] ++;
+                    }
+                }
             }
-            int k = 2;
-            while (entry[k].inum != 0)
-            {
-                ck12_inodes_used[entry[k].inum]++;
-                k++;
+            if (dip[i].addrs[NDIRECT] != 0){
+                uint* cat_indirect = (uint*) (img_ptr + BSIZE * dip[i].addrs[NDIRECT]);
+                for(int l = 0; l < BSIZE / sizeof(uint); ++ l) {
+                    if (cat_indirect[l] != 0) {
+                        struct xv6_dirent *entry = (struct xv6_dirent *) (img_ptr + cat_indirect[l] * BSIZE);
+                        for (int j = 0; j < BSIZE / sizeof(struct xv6_dirent); ++j) {
+                            if (entry[j].inum != 0 && strcmp(entry[j].name, s1) != 0 && strcmp(entry[j].name, s2) != 0)
+                                ck9_inodes_used[entry[j].inum]++;
+                        }
+
+                    }
+                }
             }
+
+
         }
     }
+//    printf("number of entries: %ld\n", dip[8].size / sizeof(struct dirent));
 
-
-    for (int i = 0; i < ninodes; ++i)
-    {
-        if (dip[i].type > 0 && ck9_inodes_used[i] == 0)
-        { // check 9
+    ck9_inodes_used[1] ++;
+    
+   
+    for (int i = 0; i < ninodes; ++ i){
+        if (dip[i].type > 0 && ck9_inodes_used[i] == 0){        // check 9
+//            printf("inode num: %d\n\n", i);
             fprintf(stderr, "ERROR: inode marked used but not found in a directory.\n");
             exit(1);
         }
-        if (dip[i].type == 0 && ck9_inodes_used[i] > 0)
-        { // check 10
+        if (dip[i].type == 0 && ck9_inodes_used[i] > 0){        // check 10
             fprintf(stderr, "ERROR: inode referred to in directory but marked free.\n");
             exit(1);
         }
-        if (dip[i].type == 2)
-        {
-            if (dip[i].nlink != ck9_inodes_used[i])
-            { // check 11
+        if(dip[i].type == 2){
+            if (dip[i].nlink != ck9_inodes_used[i]){            // check 11
                 fprintf(stderr, "ERROR: bad reference count for file.\n");
                 exit(1);
             }
         }
-        if (dip[i].type == 1)
-        {
-            if (ck12_inodes_used[i] > 1)
-            { // check 12
+
+        if(dip[i].type == 1){
+            if (ck9_inodes_used[i] > 1){                       // check 12
                 fprintf(stderr, "ERROR: directory appears more than once in file system.\n");
                 exit(1);
             }
         }
     }
 }
+/*
+void walk_dir_inode(struct dinode *dip){
+    if(dip->type != 1)
+    return ;
+
+    struct xv6_dirent *wdir;
+    for (int i=0; i<NDIRECT;i++){
+        if(dip->addrs[i]!=0){
+            wdir = (struct xv6_dirent *)(img_ptr + (dip->addrs[i]) * BSIZE);
+            for(int j = 0; j< 32; j++){
+                if(wdir->inum != 0){
+                    printf("name:%s, num %d\n", wdir->name, wdir->inum);
+                    ck9_inodes_used[wdir->inum]++;
+                    wdir ++;
+                }
+            }
+        }
+    }
+
+    if(dip->addrs[NDIRECT]==0) return;
+    uint *wdir_indirect = (uint *)(img_ptr + (dip->addrs[NDIRECT]) * BSIZE);
+    for (int i =0; i<128;i++){
+        if(*wdir_indirect != 0){
+            wdir = (struct xv6_dirent *)(img_ptr + (*wdir_indirect) * BSIZE);
+            if(wdir->inum != 0){
+                printf("name:%s, num %d \n", wdir->name, wdir->inum);
+                ck9_inodes_used[wdir->inum]++;
+                wdir++;
+            }
+        }
+        wdir_indirect++;
+    }
+}
+*/
